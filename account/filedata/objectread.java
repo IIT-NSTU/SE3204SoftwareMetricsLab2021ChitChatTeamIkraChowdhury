13d6801e548aa47758d936f719a6e45176148edb
IshratJahanRintu <86081355+IshratJahanRintu@users.noreply.github.com>
2021-10-08 02:15:52 +0600

    Ishrat rintu commit-2

---
+++ account/filedata/objectread.java
@@ -0,0 +1,67 @@
+package filedata;
+
+import User.userstructure;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.net.InetAddress;
+import javax.swing.ImageIcon;
+
+public class objectread {
+
+    public String filename, username, nickname, email;
+    userstructure user;
+    InetAddress inetAddress;
+
+    public objectread(String filename) {
+        this.filename = filename;
+        readobjectuser();
+        readobjectip();
+        userallobjectpass();
+    }
+
+    public userstructure readobjectuser() {
+        try {
+            File f = new File(filename);
+            FileInputStream file = new FileInputStream(f);
+            if (f.exists()) {
+                ObjectInputStream objectread = new ObjectInputStream(file);
+                user = (userstructure) objectread.readObject();
+                objectread.close();
+            } 
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return user;
+
+    }
+
+    public InetAddress readobjectip() {
+        try {
+            FileInputStream file = new FileInputStream(filename);
+            ObjectInputStream objectread = new ObjectInputStream(file);
+            user = (userstructure) objectread.readObject();
+            inetAddress = (InetAddress) objectread.readObject();
+            objectread.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return inetAddress;
+
+    }
+
+    public String userallobject(String username, String nickname, String email) {
+
+        this.username = username;
+        this.nickname = nickname;
+        this.email = email;
+        return username + nickname + email;
+
+    }
+
+    public void userallobjectpass() {
+        userallobject(user.getUsername(), user.getNick(), user.getemail());
+    }
+}
